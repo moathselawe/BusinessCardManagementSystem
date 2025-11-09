10b292a74d5429771fda274b1c16e3a7fc72fbce
Moath Abuqiyas <moathselawe12@gmail.com>
2025-11-09 20:08:01 +0400

    Add DeleteBusinessCardHandler

---
+++ Services/BusinessCardManagementSystem/BCMS.Application/Commands/BusinessCard/DeleteBusinessCardHandler.cs
@@ -0,0 +1,24 @@
+﻿namespace BCMS.Application.Commands.BusinessCard;
+public record DeleteBusinessCardCommand(Guid Id) : IRequest<DeleteBusinessCardResult>;
+public record DeleteBusinessCardResult(bool IsSuccess);
+public class DeleteBusinessCardHandler : IRequestHandler<DeleteBusinessCardCommand, DeleteBusinessCardResult>
+{
+    private readonly IBusinessCardRepository _businessCardRepository;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public DeleteBusinessCardHandler(IBusinessCardRepository businessCardRepository, IUnitOfWork unitOfWork)
+    {
+        _businessCardRepository = businessCardRepository;
+        _unitOfWork = unitOfWork;
+    }
+    public async Task<DeleteBusinessCardResult> Handle(DeleteBusinessCardCommand request, CancellationToken cancellationToken)
+    {
+        var success = await _businessCardRepository.DeleteAsync(request.Id, cancellationToken);
+
+        if (success)
+            await _unitOfWork.SaveWorkAsync(cancellationToken);
+
+        return new DeleteBusinessCardResult(success);
+    }
+}
+
