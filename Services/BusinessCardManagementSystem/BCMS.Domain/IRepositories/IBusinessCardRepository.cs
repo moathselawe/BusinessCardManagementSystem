2d6827ec0a7d61168b1f249712b8a623e0e9e95e
Moath Abuqiyas <moathselawe12@gmail.com>
2025-11-09 20:07:22 +0400

    Add BusinessCard Repository

---
+++ Services/BusinessCardManagementSystem/BCMS.Domain/IRepositories/IBusinessCardRepository.cs
@@ -0,0 +1,16 @@
+﻿using BCMS.Domain.Dtos;
+using System.Threading;
+
+namespace BCMS.Domain.IRepositories;
+
+public interface IBusinessCardRepository : IRepository<BusinessCard>
+{
+    Task<SearchFiltersRsDto<BusinessCardDto>> SearchAsync(SearchFiltersRqDto filters, CancellationToken cancellationToken);
+    Task<List<BusinessCard>> GetAllAsync(CancellationToken cancellationToken);
+    Task<Guid> AddAsync(BusinessCard businessCard, CancellationToken cancellationToken);
+    public Task<int> AddManyAsync(List<BusinessCard> businessCards, CancellationToken cancellationToken);
+    Task<BusinessCard?> GetByIdAsync(Guid id, CancellationToken cancellationToken);
+    Task<bool> UpdateAsync(BusinessCard businessCard, CancellationToken cancellationToken);
+    Task<bool> DeleteAsync(Guid id, CancellationToken cancellationToken);
+    Task<List<BusinessCard>> GetByIdsAsync(List<Guid> ids, CancellationToken cancellationToken);
+}
